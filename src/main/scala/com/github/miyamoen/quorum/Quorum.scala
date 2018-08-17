939fd6d6ceaf6ffa860bb7ed2d79b93fe40d04c9
miyamoen <akihito.biblioteca@gmail.com>
2018-08-17 14:06:06 +0900

    条件文を全部メソッドにした

--- src/main/scala/com/github/miyamoen/quorum/Quorum.scala
+++ src/main/scala/com/github/miyamoen/quorum/Quorum.scala
@@ -35,13 +35,22 @@ object Quorum {
 
   case object Empty extends Data
 
-  case class LockCount(writeMessage: Option[String], rest: List[ActorRef], locked: List[ActorRef], replyTo: ActorRef) extends Data
+  case class LockCount(writeMessage: Option[String], rest: List[ActorRef], locked: List[ActorRef], replyTo: ActorRef) extends Data {
+    def isComplete(stores: List[ActorRef]) : Boolean = rest.isEmpty && locked.size + 1 == stores.size
+    def hasLockedStores : Boolean = locked.nonEmpty
+  }
 
-  case class ReleaseCount(count: Int, writeMessage: Option[String], replyTo: ActorRef) extends Data
+  case class ReleaseCount(count: Int, writeMessage: Option[String], replyTo: ActorRef) extends Data {
+    def isComplete : Boolean = count - 1 == 0
+  }
 
-  case class Messages(messages: List[Message], replyTo: ActorRef) extends Data
+  case class MessageCount(messages: List[Message], replyTo: ActorRef) extends Data {
+    def isComplete(stores: List[ActorRef]) : Boolean = messages.size + 1 >= stores.size
+  }
 
-  case class WriteCount(count: Int, replyTo: ActorRef) extends Data
+  case class WriteCount(count: Int, replyTo: ActorRef) extends Data {
+    def isComplete(stores : List[ActorRef]) : Boolean = count + 1 == stores.size
+  }
 
 }
 
@@ -60,13 +69,11 @@ class Quorum(stores: List[ActorRef])
       goto(Locking) using LockCount(writeMessage = Some(message), rest = stores.tail, locked = Nil, sender())
   }
   when(Locking) {
-    case Event(Store.Succeeded(_), LockCount(None, rest, locked, replyTo))
-      if rest.isEmpty && locked.size + 1 == stores.size =>
+    case Event(Store.Succeeded(_), lockCount@LockCount(None, _, _, replyTo)) if lockCount.isComplete(stores) =>
       log.debug("Quorum finish lock to Read")
-      goto(Reading) using Messages(Nil, replyTo)
+      goto(Reading) using MessageCount(Nil, replyTo)
 
-    case Event(Store.Succeeded(_), LockCount(Some(_), rest, locked, replyTo))
-      if rest.isEmpty && locked.size + 1 == stores.size =>
+    case Event(Store.Succeeded(_), lockCount@LockCount(Some(_), _, _, replyTo)) if lockCount.isComplete(stores) =>
       log.debug("Quorum finish lock to Write")
       goto(Writing) using WriteCount(0, replyTo)
 
@@ -74,16 +81,16 @@ class Quorum(stores: List[ActorRef])
       rest.head ! Store.Lock
       stay() using LockCount(writeMessage, rest = rest.tail, locked = sender() :: locked, replyTo)
 
-    case Event(Store.Failed(_), LockCount(writeMessage, _, locked, replyTo)) if locked.nonEmpty =>
+    case Event(Store.Failed(_), lockCount@LockCount(writeMessage, _, locked, replyTo)) if lockCount.hasLockedStores =>
       log.debug("Quorum locked store count: {}", locked.size)
       goto(Releasing) using ReleaseCount(locked.size, writeMessage, replyTo)
 
-    case Event(Store.Failed(_), LockCount(writeMessage, _, locked, replyTo)) if locked.isEmpty =>
+    case Event(Store.Failed(_), lockCount@LockCount(writeMessage, _, _, replyTo)) if !lockCount.hasLockedStores =>
       goto(Locking) using LockCount(writeMessage, rest = stores.tail, locked = Nil, replyTo)
   }
 
   when(Releasing) {
-    case Event(_: Store.Result, ReleaseCount(count, writeMessage, replyTo)) if count - 1 == 0 =>
+    case Event(_: Store.Result, releaseCount@ReleaseCount(_, writeMessage, replyTo)) if releaseCount.isComplete =>
       log.debug("Quorum finish release")
       goto(Locking) using LockCount(writeMessage, rest = stores.tail, locked = Nil, replyTo)
 
@@ -92,16 +99,16 @@ class Quorum(stores: List[ActorRef])
   }
 
   when(Reading) {
-    case Event(message: Message, Messages(messages, replyTo)) if messages.size + 1 >= stores.size =>
+    case Event(message: Message, messageCount@MessageCount(messages, replyTo)) if messageCount.isComplete(stores) =>
       replyTo ! (message :: messages).maxBy(msg => msg.timestamp)
       goto(Open) using Empty
 
-    case Event(message: Message, Messages(messages, replyTo)) =>
-      stay() using Messages(message :: messages, replyTo)
+    case Event(message: Message, MessageCount(messages, replyTo)) =>
+      stay() using MessageCount(message :: messages, replyTo)
   }
 
   when(Writing) {
-    case Event(Store.Succeeded(_), WriteCount(count, replyTo)) if count + 1 == stores.size =>
+    case Event(Store.Succeeded(_), writeCount@WriteCount(_, replyTo)) if writeCount.isComplete(stores) =>
       replyTo ! Succeeded
       goto(Open) using Empty
 
